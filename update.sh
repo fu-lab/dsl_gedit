839493364769f5a84846b547c7043d46c1177369
Andrew Boltachev <andrewboltachev@gmail.com>
2014-02-21 12:49:29 +0400

    сделал файл обновления

---
+++ update.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.local/share/gtksourceview-3.0/styles
+mkdir -p ~/.local/share/gtksourceview-3.0/language-specs
+cp dsl.lang -t ~/.local/share/gtksourceview-3.0/language-specs
+cp dslstyle.xml -t ~/.local/share/gtksourceview-3.0/styles
