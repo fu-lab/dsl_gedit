0f744df9f5a22d0837774affe0f01f6d1b957940
Andrew Boltachev <andrewboltachev@gmail.com>
2014-02-21 14:40:57 +0400

    вторая версия готова

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
 mkdir -p ~/.local/share/gtksourceview-3.0/styles
 mkdir -p ~/.local/share/gtksourceview-3.0/language-specs
+DIR2=~/.local/share/gtksourceview-2.0
+if [ ! -l $DIR2 && ! -l $DIR2 ];
+    ln -s ~/.local/share/gtksourceview-3.0 $DIR2
+fi
 cp dsl.lang -t ~/.local/share/gtksourceview-3.0/language-specs
 cp dslstyle.xml -t ~/.local/share/gtksourceview-3.0/styles
