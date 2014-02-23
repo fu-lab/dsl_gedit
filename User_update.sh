4955635bb094117f26910f8b7137f60ab4a76a97
Andrey Chemyshev <chemyshev.andrey@gmail.com>
2014-02-23 17:14:55 +0400

    Andrey Chemyshev commit

--- User_update.sh
+++ User_update.sh
@@ -4,5 +4,5 @@ DIR2=~/.local/share/gtksourceview-2.0
 #if [ ! -l $DIR2 && ! -l $DIR2 ];
     ln -s ~/.local/share/gtksourceview-3.0 $DIR2
 #fi
-ln -s `pwd`/dsl.lang -t ~/.local/share/gtksourceview-3.0/language-specs
-ln -s `pwd`/dslstyle.xml -t ~/.local/share/gtksourceview-3.0/styles
+cp dsl.lang -t ~/.local/share/gtksourceview-3.0/language-specs
+cp dslstyle.xml -t ~/.local/share/gtksourceview-3.0/styles
